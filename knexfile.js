81194d698d826e10e63c9a15af57e06ca34a8b20
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-04-29 23:52:34 -0400

    Removed SQLite3

--- knexfile.js
+++ knexfile.js
@@ -9,21 +9,18 @@ const dbConnection = process.env.DATABASE_URL || localPgConnection;
 
 module.exports = {
   development: {
-    client: "sqlite3",
-    connection: {
-      filename: "./data/instagram.sqlite3"
+    client: "pg",
+    connection: dbConnection + "?ssl=true",
+    pool: {
+      min: 2,
+      max: 10
     },
-    useNullAsDefault: true,
     migrations: {
+      tableName: "knex_migrations",
       directory: "./data/migrations"
     },
     seeds: {
       directory: "./data/seeds"
-    },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run("PRAGMA foreign_keys = ON", done); // enforce FK
-      }
     }
   },
   production: {
