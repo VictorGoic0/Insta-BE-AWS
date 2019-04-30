b7498c7a69cb96633c2ce512424a237702436ba9
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-04-30 00:33:38 -0400

    Refactored knexfile to connect to RDS.

--- knexfile.js
+++ knexfile.js
@@ -2,11 +2,11 @@ module.exports = {
   development: {
     client: "pg",
     connection: {
-      host: process.env.DB_TEST_HOST || "localhost",
-      database: process.env.DB_TEST_DATABASE || "rxid",
-      user: process.env.DB_TEST_USER || "admin",
-      password: process.env.DB_TEST_PASSWORD || "pass",
-      port: process.env.DB_TEST_PORT || "5432"
+      host: process.env.DB_DEV_HOST || "localhost",
+      database: process.env.DB_DEV_DATABASE || "rxid",
+      user: process.env.DB_DEV_USER || "admin",
+      password: process.env.DB_DEV_PASSWORD || "pass",
+      port: process.env.DB_DEV_PORT || "5432"
     },
     pool: {
       min: 2,
@@ -22,7 +22,13 @@ module.exports = {
   },
   production: {
     client: "pg",
-    connection: dbConnection + "?ssl=true",
+    connection: {
+      host: process.env.DB_PROD_HOST || "localhost",
+      database: process.env.DB_PROD_DATABASE || "rxid",
+      user: process.env.DB_PROD_USER || "admin",
+      password: process.env.DB_PROD_PASSWORD || "pass",
+      port: process.env.DB_PROD_PORT || "5432"
+    },
     pool: {
       min: 2,
       max: 10
